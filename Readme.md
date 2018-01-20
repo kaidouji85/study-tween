cd06a95248a896f1db67d09422e2283990a3b47d
竹内佑介 <kaidouji85@gmail.com>
2018-01-20 20:00:29 +0900

    ファーストコミット

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# tween.js動作検証
+このレポジトリは[tween.js](https://github.com/tweenjs/tween.js/)の動作検証をするためのものです。
+
+## セットアップ方法
+```
+npm install
+npm start -- src/multi-tween.js
+```
+
+## 特定ファイルの実行方法
+```
+npm start -- <実行したいHTMLのパス>
+```
+※実行できるファイルはsrcフォルダ配下のHTMLです。
---
+++ src/multi-tween.js
@@ -0,0 +1,35 @@
+/**
+ * tweenの並列実行を確認している。
+ * このサンプルでtweenは以下のように振舞う
+ *
+ * (1)tweenAを実行
+ * (2)tweenB、tweenCが並行実行
+ */
+import TWEEN from '@tweenjs/tween.js';
+
+const target = {a: 0, b: 0, c: 0};
+const logTime = () => new Date().toString();
+
+function gameLoop(time) {
+  requestAnimationFrame(gameLoop);
+  TWEEN.update(time);
+}
+requestAnimationFrame(gameLoop);
+
+const tweenA = new TWEEN.Tween(target)
+  .to({a: 1000}, 1000)
+  .onUpdate(() => console.log(`${logTime()} tweenA {a:${target.a}, b:${target.b} c:${target.c}}`))
+  .onComplete(() => console.log(`${logTime()} tweenA complete!`));
+
+const tweenB = new TWEEN.Tween(target)
+  .to({b: 1000}, 1000)
+  .onUpdate(() => console.log(`${logTime()} tweenB {a:${target.a}, b:${target.b} c:${target.c}}`))
+  .onComplete(() => console.log(`${logTime()} tweenB complete!`));
+
+const tweenC = new TWEEN.Tween(target)
+  .to({c: 1000}, 1000)
+  .onUpdate(() => console.log(`${logTime()} tweenC {a:${target.a}, b:${target.b} c:${target.c}}`))
+  .onComplete(() => console.log(`${logTime()} tweenC complete!`));
+
+tweenA.chain(tweenB, tweenC);
+tweenA.start();
